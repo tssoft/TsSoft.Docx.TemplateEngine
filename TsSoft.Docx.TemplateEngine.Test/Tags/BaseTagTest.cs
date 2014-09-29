7825f3831695f97fc76280e7c5d80a77ea727151
Georgy <georgy.polikarpov@mail.ru>
2014-09-29 13:07:30 +0400

    ValidateTagsRemove

--- TsSoft.Docx.TemplateEngine.Test/Tags/BaseTagTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/BaseTagTest.cs
@@ -1,11 +1,25 @@
-﻿using System.Xml;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
 
 namespace TsSoft.Docx.TemplateEngine.Test.Tags
 {
     public class BaseTagTest
     {
+        public static readonly XNamespace WordMlNamespace = "http://schemas.openxmlformats.org/wordprocessingml/2006/main";
+
+        /// <summary>
+        /// Check for number of tags is 0
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <author>Георгий Поликарпов</author>
         protected void ValidateTagsRemoved(XmlDocument doc)
         {
+            XDocument document = XDocument.Load(new XmlNodeReader(doc));
+            IEnumerable<XElement> tags = document.Descendants(WordMlNamespace + "sdt");
+            Assert.IsFalse(tags.Any());
         }
     }
 }
