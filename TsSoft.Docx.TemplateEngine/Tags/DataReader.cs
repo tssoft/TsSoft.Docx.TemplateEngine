aea83bcc4f90669eb8382d3342195d4e28825d07
Victor Glushenkov <v.glushenkov@ts-soft.ru>
2014-09-29 19:12:03 +0400

    [Text] processor

---
+++ TsSoft.Docx.TemplateEngine/Tags/DataReader.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Linq;
+using System.Xml.Serialization;
+using System.Xml.XPath;
+
+namespace TsSoft.Docx.TemplateEngine.Tags
+{
+    internal class DataReader<E>
+    {
+        private XDocument dataDocument;
+
+        public DataReader(E dataEntity)
+        {
+            dataDocument = new XDocument();
+            using (var writer = dataDocument.CreateWriter())
+            {
+                var serializer = new XmlSerializer(typeof(E));
+                serializer.Serialize(writer, dataEntity);
+            }
+        }
+
+        public string ReadText(string expression)
+        {
+            var textElement = dataDocument.XPathSelectElement(expression);
+            return textElement.Value;
+        }
+    }
+}
