fd255aa25bea200a45117d619e8a087f62280ad0
Georgy Polikarpov <g.polikarpov@ts-soft.ru>
2014-10-06 19:27:14 +0400

    TemplateEngine Demo

---
+++ TsSoft.Docx.TemplateEngine.Demo/TsSoft.Docx.TemplateEngine.Demo/Program.cs
@@ -0,0 +1,55 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Xml.Linq;
+
+namespace TsSoft.Docx.TemplateEngine.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (!Parser.Default.ParseArguments(args, options) || options.Help)
+            {
+                Console.WriteLine(options.GetUsage());
+            }
+            else if (CanGenerate(options))
+            {
+                Generate(options);
+            }
+        }
+
+        private static void Generate(Options options)
+        {
+            using (var templateStream = new FileStream(options.TemplateFileName, FileMode.Open))
+            using (var destinationStream = new FileStream(options.TargetFileName, FileMode.Create))
+            {
+                var dataDocument = XDocument.Load(options.DataFileName);
+                var generator = new DocxGenerator();
+                try
+                {
+                    generator.GenerateDocx(templateStream, destinationStream, dataDocument);
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine("Error: {0}", exception.Message);
+                }
+            }
+        }
+
+        private static bool CanGenerate(Options options)
+        {
+            if (!File.Exists(options.TemplateFileName))
+            {
+                Console.WriteLine("Template file {0} not found", options.TemplateFileName);
+            }
+            if (!File.Exists(options.DataFileName))
+            {
+                Console.WriteLine("Data file {0} not found", options.DataFileName);
+            }
+
+            return (File.Exists(options.TemplateFileName) && File.Exists(options.DataFileName));
+        }
+    }
+}
