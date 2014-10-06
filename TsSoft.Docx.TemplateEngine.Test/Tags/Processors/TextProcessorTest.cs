e8403dcdd1581a6d223ec3107eba118dea81ecd0
Ivan Ananyev <i.ananyev@ts-soft.ru>
2014-10-06 16:54:47 +0400

    introduce TextProcessorTest.cs

--- TsSoft.Docx.TemplateEngine.Test/Tags/Processors/TextProcessorTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/Processors/TextProcessorTest.cs
@@ -1,15 +1,39 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Reflection;
-
+using System.Xml.Linq;
+using TsSoft.Docx.TemplateEngine.Tags;
+using TsSoft.Docx.TemplateEngine.Tags.Processors;
 namespace TsSoft.Docx.TemplateEngine.Test.Tags.Processors
 {
+    using System.Linq;
+
     [TestClass]
     public class TextProcessorTest : BaseProcessorTest
     {
         [TestMethod]
         public void TestProcess()
         {
-           
+            var textElement = new XElement("text");
+            const string DynamicText = "Some people just want to watch the world burn.";
+            textElement.Value = DynamicText;
+            var data = new XElement("data", new XElement("test", textElement));
+
+            var textTag = new XElement(WordMl.SdtName, "Text");
+            var document = new XElement("body", textTag);
+
+            var processor = new TextProcessor
+            {
+                DataReader = new DataReader(data),
+                TextTag = new TextTag
+                              {
+                                  Expression = "//test/text",
+                                  TagNode = textTag
+                              }
+            };
+
+            processor.Process();
+
+            this.ValidateTagsRemoved(document);
+            Assert.IsNotNull(document.Descendants(WordMl.RName).Single(d => d.Value == DynamicText));
         }
     }
 }
