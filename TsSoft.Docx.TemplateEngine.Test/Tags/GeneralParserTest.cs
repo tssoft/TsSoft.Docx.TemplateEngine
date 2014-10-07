5d1b0ef9247b02e0926e3176c95bbe18cc5e8324
v.moroz <v.moroz@ts-soft.ru>
2014-10-07 20:06:23 +0400

    23 "GeneralParserTest"

--- TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
@@ -43,42 +43,83 @@ namespace TsSoft.Docx.TemplateEngine.Test.Tags
         [TestInitialize]
         public void Init()
         {
-            generalParser = new GeneralParser();
+            generalParser = new GeneralParser();            
+        }
+
+        [TestMethod]
+        public void TestParseTableNested()
+        {
+            var docStream = AssemblyResourceHelper.GetResourceStream(this, "if_in_table_document.xml");
+            doc = XDocument.Load(docStream);
+
+            var expectedRootProcessor = new RootProcessor();
+            ITagProcessor expectedTableProcessor = new TableProcessor();
+            expectedTableProcessor.AddProcessor(new IfProcessor());
+            IList<ITagProcessor> expectedTableProcessors = new List<ITagProcessor>(expectedTableProcessor.Processors);
+            expectedRootProcessor.AddProcessor(expectedRootProcessor);
+            
+            IList<ITagProcessor> expectedRootProcessors = new List<ITagProcessor>(expectedRootProcessor.Processors);
+            
+            ITagProcessor actualRootProcessor = new RootProcessor();
+            generalParser.Parse(actualRootProcessor, doc.Root);
+            IList<ITagProcessor> actualRootProcessors = new List<ITagProcessor>();
+
+            Assert.AreEqual(expectedRootProcessors.Count, actualRootProcessors.Count);
+            Assert.AreEqual(expectedRootProcessors[0].GetType(), actualRootProcessors[0].GetType());
             
 
+        }
+
+
+        [TestMethod]
+        public void TestParseIfNested()
+        {
+            var docStream = AssemblyResourceHelper.GetResourceStream(this, "text_in_if_document.xml");
+            doc = XDocument.Load(docStream);
+            
+            var expectedRootProcessor = new RootProcessor();
+            ITagProcessor expectedIfProcessor = new IfProcessor();
+            
+            expectedIfProcessor.AddProcessor(new TextProcessor());
+            IList<ITagProcessor> expectedIfProcessors = new List<ITagProcessor>(expectedIfProcessor.Processors);
+            expectedRootProcessor.AddProcessor(expectedIfProcessor);            
+            IList<ITagProcessor> expectedRootProcessors = new List<ITagProcessor>(expectedRootProcessor.Processors);
+
+            ITagProcessor actualRootProcessor = new RootProcessor();
+            generalParser.Parse(actualRootProcessor, doc.Root);
+
+            IList<ITagProcessor> actualRootProcessors = new List<ITagProcessor>(actualRootProcessor.Processors);
+            Assert.AreEqual(expectedRootProcessors.Count, actualRootProcessors.Count);
+            Assert.AreEqual(expectedRootProcessors[0].GetType(),actualRootProcessors[0].GetType());
+            IList<ITagProcessor> actualIfProcessors = new List<ITagProcessor>(actualRootProcessors[0].Processors);
+            Assert.AreEqual(expectedIfProcessors.Count, actualIfProcessors.Count);
+            Assert.AreEqual(expectedIfProcessors[0].GetType(), actualIfProcessors[0].GetType());
+            Assert.AreEqual(expectedIfProcessors[0].Processors.Count, expectedIfProcessors[0].Processors.Count); 
+             
         }
 
         [TestMethod]
         public void TestParseRepeaterNested()
         {  
-            /*
-            generalParser = new GeneralParser();
-            var docStream = AssemblyResourceHelper.GetResourceStream(this, "table_document.xml");
-            doc = XDocument.Load(docStream);
-            */
 
             var docStream = AssemblyResourceHelper.GetResourceStream(this, "text_in_repeater_document.xml");
             doc = XDocument.Load(docStream);
 
-            // Ожидаемое дерево процессов
             var expectedRootProcessor = new RootProcessor();
+
             ITagProcessor expectedRepeaterProcessor = new RepeaterProcessor();
             expectedRepeaterProcessor.AddProcessor(new TextProcessor());
             IList<ITagProcessor> expectedRepeaterProcessors = new List<ITagProcessor>(expectedRepeaterProcessor.Processors);
-            expectedRootProcessor.AddProcessor(expectedRepeaterProcessor);   
-            // --- 
+            expectedRootProcessor.AddProcessor(expectedRepeaterProcessor);               
             IList<ITagProcessor> expectedRootProcessors = new List<ITagProcessor>(expectedRootProcessor.Processors);
 
             ITagProcessor actualRoot = new RootProcessor();
-            generalParser.Parse(actualRoot, doc.Root);
-            
-            IList<ITagProcessor> actualRootProcessors = new List<ITagProcessor>(actualRoot.Processors);
-            
+            generalParser.Parse(actualRoot, doc.Root);            
+            IList<ITagProcessor> actualRootProcessors = new List<ITagProcessor>(actualRoot.Processors);            
             Assert.AreEqual(actualRootProcessors.Count, expectedRootProcessors.Count);
-            Assert.AreEqual(expectedRootProcessors[0].GetType(), actualRootProcessors[0].GetType()); // Asserting RepeaterProc
+            Assert.AreEqual(expectedRootProcessors[0].GetType(), actualRootProcessors[0].GetType()); 
             Assert.AreEqual(expectedRootProcessors[0].Processors.Count, actualRootProcessors[0].Processors.Count);
-            IList<ITagProcessor> actualRepeaterProcessors = new List<ITagProcessor>(actualRootProcessors[0].Processors); // Asserting
-            //Assert.AreEqual(expectedRepeaterProcessor.Processors.Count, actualRepeaterProcessors.Count);            
+            IList<ITagProcessor> actualRepeaterProcessors = new List<ITagProcessor>(actualRootProcessors[0].Processors);         
             Assert.AreEqual(expectedRepeaterProcessors[0].GetType(), actualRepeaterProcessors[0].GetType());
             Assert.AreEqual(expectedRepeaterProcessors.Count, actualRepeaterProcessors.Count);
 
