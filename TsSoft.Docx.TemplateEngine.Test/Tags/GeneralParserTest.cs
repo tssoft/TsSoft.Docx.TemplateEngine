71b2de95da5209d362c098168583770c019f776a
v.moroz <v.moroz@ts-soft.ru>
2014-10-07 18:22:05 +0400

    23 "GeneralParserTest"

--- TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
@@ -1,4 +1,7 @@
-﻿using System.Xml.Linq;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TsSoft.Commons.Utils;
@@ -14,30 +17,72 @@ namespace TsSoft.Docx.TemplateEngine.Test.Tags
 
         private GeneralParser generalParser;
         private XDocument doc;
-
+        /*
         private Mock<ITagProcessor> tagProcessorMock;
         private Mock<TextParser> textParserMock;
         private Mock<TableParser> tableParserMock;
         private Mock<RepeaterParser> repeaterParserMock;
 
         private Mock<RootProcessor> rootProcessorMock;
+       */
+        /*
+        private void AssertingTree(IList<ITagProcessor> expected, IList<ITagProcessor> actual)
+        {
+            Assert.AreEqual(expected.Count, actual.Count);
+            for (int i = 0; i < expected.Count; i++)
+            {                
+                Assert.AreEqual(expected[i].GetType(), actual[i].GetType());
+               // Assert.AreEqual(expected[i].);
+                AssertingTree(new List<ITagProcessor>(actual[i].Processors),new List<ITagProcessor>(expected[i].Processors));
+            }
+        }
+         * */
+
        
+
+        [TestInitialize]
+        public void Init()
+        {
+            generalParser = new GeneralParser();
+            
+
+        }
+
         [TestMethod]
-        public void TestParse()
+        public void TestParseRepeaterNested()
         {  
+            /*
             generalParser = new GeneralParser();
             var docStream = AssemblyResourceHelper.GetResourceStream(this, "table_document.xml");
             doc = XDocument.Load(docStream);
+            */
 
-            tagProcessorMock = new Mock<ITagProcessor>();
+            var docStream = AssemblyResourceHelper.GetResourceStream(this, "text_in_repeater_document.xml");
+            doc = XDocument.Load(docStream);
 
             // Ожидаемое дерево процессов
             var expectedRootProcessor = new RootProcessor();
-            expectedRootProcessor.AddProcessor(new TableProcessor());
+            ITagProcessor expectedRepeaterProcessor = new RepeaterProcessor();
+            expectedRepeaterProcessor.AddProcessor(new TextProcessor());
+            IList<ITagProcessor> expectedRepeaterProcessors = new List<ITagProcessor>(expectedRepeaterProcessor.Processors);
+            expectedRootProcessor.AddProcessor(expectedRepeaterProcessor);   
             // --- 
-            ITagProcessor actualProcessor;
-           // rootProcessorMock.Setup(rp => rp.AddProcessor(It.IsAny<ITagProcessor>())).Callback(p => actualProcessor = p);
-           //rootProcessorMock.Object.
+            IList<ITagProcessor> expectedRootProcessors = new List<ITagProcessor>(expectedRootProcessor.Processors);
+
+            ITagProcessor actualRoot = new RootProcessor();
+            generalParser.Parse(actualRoot, doc.Root);
+            
+            IList<ITagProcessor> actualRootProcessors = new List<ITagProcessor>(actualRoot.Processors);
+            
+            Assert.AreEqual(actualRootProcessors.Count, expectedRootProcessors.Count);
+            Assert.AreEqual(expectedRootProcessors[0].GetType(), actualRootProcessors[0].GetType()); // Asserting RepeaterProc
+            Assert.AreEqual(expectedRootProcessors[0].Processors.Count, actualRootProcessors[0].Processors.Count);
+            IList<ITagProcessor> actualRepeaterProcessors = new List<ITagProcessor>(actualRootProcessors[0].Processors); // Asserting
+            //Assert.AreEqual(expectedRepeaterProcessor.Processors.Count, actualRepeaterProcessors.Count);            
+            Assert.AreEqual(expectedRepeaterProcessors[0].GetType(), actualRepeaterProcessors[0].GetType());
+            Assert.AreEqual(expectedRepeaterProcessors.Count, actualRepeaterProcessors.Count);
+
+           
         }
 
     }
