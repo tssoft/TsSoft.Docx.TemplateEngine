de5615235540e9633c6dd0b168651ba3d4a662ff
v.moroz <v.moroz@ts-soft.ru>
2014-10-07 15:47:04 +0400

    23 "GeneralParserTest"

--- TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/GeneralParserTest.cs
@@ -1,35 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
+﻿using System.Xml.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TsSoft.Commons.Utils;
+using TsSoft.Docx.TemplateEngine.Tags;
 using TsSoft.Docx.TemplateEngine.Tags.Processors;
 
-namespace TsSoft.Docx.TemplateEngine.Tags
+namespace TsSoft.Docx.TemplateEngine.Test.Tags
 {
     [TestClass]
     public class GeneralParserTest
     {
 
-        private GeneralParser generalParser;       
 
-        [TestInitialize]
-        public void Init()
-        {
-            generalParser = new GeneralParser();
-            var docStream = AssemblyResourceHelper.GetResourceStream(this, "GeneralParserTest.xml");
-            var doc = XDocument.Load(docStream);
-        }
+        private GeneralParser generalParser;
+        private XDocument doc;
+
+        private Mock<ITagProcessor> tagProcessorMock;
+        private Mock<TextParser> textParserMock;
+        private Mock<TableParser> tableParserMock;
+        private Mock<RepeaterParser> repeaterParserMock;
+
+        private Mock<RootProcessor> rootProcessorMock;
+       
         [TestMethod]
         public void TestParse()
-        {
-            var tagProcessorMock = new Mock<ITagProcessor>();
-            //generalParser.Parse(tagProcessorMock.Object,);
-            
+        {  
+            generalParser = new GeneralParser();
+            var docStream = AssemblyResourceHelper.GetResourceStream(this, "table_document.xml");
+            doc = XDocument.Load(docStream);
+
+            tagProcessorMock = new Mock<ITagProcessor>();
+
+            // Ожидаемое дерево процессов
+            var expectedRootProcessor = new RootProcessor();
+            expectedRootProcessor.AddProcessor(new TableProcessor());
+            // --- 
+            ITagProcessor actualProcessor;
+           // rootProcessorMock.Setup(rp => rp.AddProcessor(It.IsAny<ITagProcessor>())).Callback(p => actualProcessor = p);
+           //rootProcessorMock.Object.
         }
 
     }
