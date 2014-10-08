71fffdb36968fa26679f505742c9a2e04d5e9507
Ivan Ananyev <i.ananyev@ts-soft.ru>
2014-10-08 14:21:53 +0400

    clean up

--- TsSoft.Docx.TemplateEngine.Test/Tags/TraverseUtilsTest.cs
+++ TsSoft.Docx.TemplateEngine.Test/Tags/TraverseUtilsTest.cs
@@ -2,12 +2,20 @@
 
 namespace TsSoft.Docx.TemplateEngine.Test.Tags
 {
+    using System.Xml.Linq;
+
+    using TsSoft.Commons.Utils;
+
     [TestClass]
     public class TraverseUtilsTest
     {
         [TestMethod]
         public void TestNextElements()
         {
+            var docStream = AssemblyResourceHelper.GetResourceStream(this, "ComplexIf.xml");
+            var doc = XDocument.Load(docStream);
+            var documentRoot = doc.Root.Element(WordMl.WordMlNamespace + "body");
+
         }
     }
 }
