0cbf4862ade94879f374f31fbea6d2a7c30bb5e8
Ivan Ananyev <i.ananyev@ts-soft.ru>
2014-10-08 15:04:49 +0400

    TraverseUtils.NextElements fix -> search everywhere

--- TsSoft.Docx.TemplateEngine/Tags/TraverseUtils.cs
+++ TsSoft.Docx.TemplateEngine/Tags/TraverseUtils.cs
@@ -4,13 +4,24 @@ using System.Xml.Linq;
 
 namespace TsSoft.Docx.TemplateEngine.Tags
 {
+    using System;
+
     internal static class TraverseUtils
     {
+        public static IEnumerable<XElement> NextTagElements(XElement startElement)
+        {
+            return NextTagElements(startElement, null);
+        }
+
         public static IEnumerable<XElement> NextTagElements(XElement startElement, string tagName)
         {
-            return startElement.ElementsAfterSelf(WordMl.SdtName)
-                .Where(element => element.Element(WordMl.SdtPrName)
-                .Element(WordMl.TagName).Attribute(WordMl.ValAttributeName).Value.Equals(tagName));
+            var nextTagElements = startElement.ElementsAfterSelf(WordMl.SdtName).Where(MakeTagMatchingPredicate(tagName)).ToList();
+            if (!nextTagElements.Any())
+            {
+                var nextTagsInnerElements = GoDeeper(startElement, tagName).ToList();
+                return nextTagsInnerElements.Any() ? nextTagsInnerElements : GoUp(startElement, tagName);
+            }
+            return nextTagElements;
         }
 
         public static IEnumerable<XElement> TagElementsBetween(XElement startElement, XElement endElement, string tagName)
@@ -25,7 +36,7 @@ namespace TsSoft.Docx.TemplateEngine.Tags
 
         public static XElement TagElement(XElement root, string tagName)
         {
-            return root.Elements(WordMl.SdtName).First(element => element.Element(WordMl.SdtPrName).Element(WordMl.TagName).Attribute(WordMl.ValAttributeName).Value == tagName);
+            return root.Descendants(WordMl.SdtName).First(element => element.Element(WordMl.SdtPrName).Element(WordMl.TagName).Attribute(WordMl.ValAttributeName).Value == tagName);
         }
 
         public static bool IsTag(this XElement self, string tagName)
@@ -43,6 +54,37 @@ namespace TsSoft.Docx.TemplateEngine.Tags
         public static string GetExpression(this XElement self)
         {
             return self.IsSdt() ? self.Value : null;
+        }
+
+        private static Func<XElement, bool> MakeTagMatchingPredicate(string tagName)
+        {
+            Func<XElement, bool> func;
+            if (string.IsNullOrEmpty(tagName))
+            {
+                func = element => true;
+            }
+            else
+            {
+                func = element => element.Element(WordMl.SdtPrName).Element(WordMl.TagName).Attribute(WordMl.ValAttributeName).Value.Equals(tagName);
+            }
+            return func;
+        }
+
+        private static IEnumerable<XElement> GoDeeper(XElement startElement, string name)
+        {
+            return startElement.ElementsAfterSelf().Descendants(WordMl.SdtName).Where(MakeTagMatchingPredicate(name));
+        }
+
+        private static IEnumerable<XElement> GoUp(XElement startElement, string name)
+        {
+            IEnumerable<XElement> result = Enumerable.Empty<XElement>();
+            var parent = startElement.Parent;
+            while (!result.Any() && !parent.Name.Equals(WordMl.BodyName))
+            {
+                result = parent.ElementsAfterSelf().DescendantsAndSelf(WordMl.SdtName).Where(MakeTagMatchingPredicate(name));
+                parent = parent.Parent;
+            }
+            return result;
         }
     }
 }
