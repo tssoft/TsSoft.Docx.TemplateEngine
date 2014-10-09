111ddbea2776691b5f254733e10bc068affd15f3
v.moroz <v.moroz@ts-soft.ru>
2014-10-09 14:19:11 +0400

    TextRunProperties Adding

--- TsSoft.Docx.TemplateEngine/WordMl.cs
+++ TsSoft.Docx.TemplateEngine/WordMl.cs
@@ -31,47 +31,5 @@ namespace TsSoft.Docx.TemplateEngine
         public static readonly XName ProofingErrorAnchorName = WordMlNamespace + "proofErr";
         public static readonly XName BookmarkStartName = WordMlNamespace + "bookmarkStart";
 
-        public static readonly XName ReferenceStyleName = WordMlNamespace + "rStyle";
-        public static readonly XName RunFontsName = WordMlNamespace + "rFonts";
-        public static readonly XName BoldName = WordMlNamespace + "b";
-        public static readonly XName ComplexScriptBoldName = WordMlNamespace + "bCs";
-        public static readonly XName ItalicsName = WordMlNamespace + "i";
-        public static readonly XName ComplexScriptItalicsName = WordMlNamespace + "iCs";
-        public static readonly XName CapsName = WordMlNamespace + "caps";
-        public static readonly XName SmallCapsName = WordMlNamespace + "smallCaps";
-        public static readonly XName StrikeName = WordMlNamespace + "strike";
-        public static readonly XName DoubleStrikeName = WordMlNamespace + "dstrike";
-        public static readonly XName OutlineName = WordMlNamespace + "outline";
-        public static readonly XName ShadowName = WordMlNamespace + "shadow";
-        public static readonly XName EmbossName = WordMlNamespace + "emboss";
-        public static readonly XName ImprintName = WordMlNamespace + "imprint";
-        public static readonly XName NoProofName = WordMlNamespace + "noProof";
-        public static readonly XName SnapToGridName = WordMlNamespace + "snapToGrid";
-        public static readonly XName VanishName = WordMlNamespace + "vanish";
-        public static readonly XName WebHiddenName = WordMlNamespace + "webHidden";
-        public static readonly XName ColorName = WordMlNamespace + "color";
-        public static readonly XName SpacingName = WordMlNamespace + "spacing";
-        public static readonly XName WName = WordMlNamespace + "w";
-        public static readonly XName KernName = WordMlNamespace + "kern";
-        public static readonly XName PositionName = WordMlNamespace + "position";
-        public static readonly XName FontSizeName = WordMlNamespace + "sz";
-        public static readonly XName ComplexScriptFontSizeName = WordMlNamespace + "szCs";
-        public static readonly XName HighlightTextName = WordMlNamespace + "highlight";
-        public static readonly XName UnderlineName = WordMlNamespace + "u";
-        public static readonly XName EffectName = WordMlNamespace + "effect";
-        public static readonly XName TextBorderName = WordMlNamespace + "bdr";
-        public static readonly XName RunShadingName = WordMlNamespace + "shd";
-        public static readonly XName FitTextName = WordMlNamespace + "fitText";
-        public static readonly XName VertAlignName = WordMlNamespace + "vertAlign";
-        public static readonly XName RightToLeftTextName = WordMlNamespace + "rtl";
-        public static readonly XName ComplexScriptName = WordMlNamespace + "cs";
-        public static readonly XName EmphasisMarkName = WordMlNamespace + "em";
-        public static readonly XName LanguageName = WordMlNamespace + "lang";
-        public static readonly XName EastAsianLayoutName = WordMlNamespace + "eastAsianLayout";
-        public static readonly XName SpecVanishName = WordMlNamespace + "specVanish";
-        public static readonly XName OfficeOpenXmlMath = WordMlNamespace + "oMath";
-        
-
-
     }
 }
