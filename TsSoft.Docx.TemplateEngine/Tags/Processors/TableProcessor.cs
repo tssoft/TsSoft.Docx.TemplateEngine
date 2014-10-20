7d3cd1647c1aaddb9a8bc16b2e6fed5a380cf50a
Georgy Polikarpov <g.polikarpov@ts-soft.ru>
2014-10-20 14:11:29 +0400

    TableProcessor: ItemIf false condition branch fixed

--- TsSoft.Docx.TemplateEngine/Tags/Processors/TableProcessor.cs
+++ TsSoft.Docx.TemplateEngine/Tags/Processors/TableProcessor.cs
@@ -49,98 +49,128 @@ namespace TsSoft.Docx.TemplateEngine.Tags.Processors
             XElement firstCell = null;
             for (int listIndex = 0; listIndex < tableElementsList.Count; listIndex++)
             {
-                string resultText = null;
                 var currentTableElement = tableElementsList[listIndex];
-                if (currentTableElement.IsItem)
+                if (currentTableElement.IsItemIf)
                 {
-                    resultText = dataReader.ReadText(currentTableElement.Expression);
+                    previous = this.ProcessItemIfElement(currentTableElement, dataReader, index, previous);
                 }
-                else if (currentTableElement.IsItemIf)
+                else if (currentTableElement.IsItem || currentTableElement.IsIndex)
                 {
-                    bool condition;
-                    bool.TryParse(dataReader.ReadText(currentTableElement.Expression), out condition);
-                    if (condition)
-                    {
-                        this.ProcessElements(currentTableElement.TagElements, dataReader, index, previous, false);
-                        var currentCell = currentTableElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName);
-                        if (previous != null && !previous.Equals(currentCell))
-                        {
-                            currentCell.Remove();
-                            previous.AddAfterSelf(currentCell);
-                        }
-                        previous = currentTableElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName);
-                        currentTableElement.StartTag.Remove();
-                        currentTableElement.EndTag.Remove();
-                    }
-                    else
-                    {
-                        var currentCell = currentTableElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName);                        
-                        currentCell.Add(new XElement(WordMl.ParagraphName));                        
-                        if (previous != null && !previous.Equals(currentCell))
-                        {
-                            currentCell.Remove();
-                            previous.AddAfterSelf(currentCell);
-                        }
-                        previous = currentCell;
-                        this.CleanUp(currentTableElement.StartTag, currentTableElement.EndTag);
-                    }
+                    var resultText = currentTableElement.IsIndex
+                                     ? index.ToString(CultureInfo.CurrentCulture)
+                                     : dataReader.ReadText(currentTableElement.Expression);
 
-                }
-                else if (currentTableElement.IsIndex)
-                {
-                    resultText = index.ToString(CultureInfo.CurrentCulture);
-                }
-                if (resultText != null)
-                {
                     previous = this.ProcessCell(currentTableElement, previous, resultText);
                     if (firstCell == null)
                     {
                         firstCell = previous;
                     }
-                    
+
                     currentTableElement.StartTag.Remove();
                 }
             }
 
             if (isTopLevel && firstCell != null)
             {
-                var staticCells = firstCell.ElementsBeforeSelf(WordMl.TableCellName).ToList();
-                if (staticCells.Any())
+                ProcessStaticCells(firstCell, previous);
+            }
+        }
+
+        private XElement ProcessItemIfElement(TableElement itemIfElement, DataReader dataReader, int index, XElement previous)
+        {
+            bool condition;
+            bool.TryParse(dataReader.ReadText(itemIfElement.Expression), out condition);
+            var currentCell = itemIfElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName); 
+            if (condition)
+            {
+                this.ProcessElements(itemIfElement.TagElements, dataReader, index, previous, false);
+                itemIfElement.StartTag.Remove();
+                itemIfElement.EndTag.Remove();
+
+                if (previous != null && !previous.Equals(currentCell))
                 {
-                    staticCells.Remove();
-                    previous.AddAfterSelf(staticCells);
+                    currentCell.Remove();
+                    previous.AddAfterSelf(currentCell);
                 }
             }
+            else
+            {
+                this.CleanUp(itemIfElement.StartTag, itemIfElement.EndTag);
+                if (!currentCell.Elements(WordMl.ParagraphName).Any())
+                {
+                    currentCell.Add(new XElement(WordMl.ParagraphName));
+                }
+                  
+                if (previous != null && !previous.Equals(currentCell))
+                {
+                    currentCell.Remove();
+                    previous.AddAfterSelf(currentCell);
+                }
+                else if (previous == null)
+                {
+                    var parentRow = currentCell.Parent;
+                    currentCell.Remove();
+                    parentRow.Add(currentCell);
+                }
+            }
+
+            return currentCell;
         }
 
         private XElement ProcessCell(TableElement tableElement, XElement previous, string text)
         {
-            var isInnerCell = true;
-            var currentCell = tableElement.StartTag.Descendants(WordMl.TableCellName).FirstOrDefault();
-            if (currentCell == null)
-            {
-                currentCell = tableElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName);
-                isInnerCell = false;
-            }
+            var isInnerCell = tableElement.HasCell();
+            var currentCell = this.CurrentCell(tableElement);
+            previous = this.ProcessStaticCells(tableElement, previous);
 
-            XElement parent = null;
-            if (isInnerCell)
-            {
-                parent = currentCell;
-            }
-            else
-            {
-             //   parent = ( currentCell.Elements(WordMl.ParagraphName).Any()) ? currentCell.Element(WordMl.ParagraphName) : tableElement.StartTag;
-                parent = tableElement.StartTag;
-            }
+            var parent = isInnerCell ? currentCell: tableElement.StartTag.Parent;
             var result = DocxHelper.CreateTextElement(
                 tableElement.StartTag,
                 parent,
                 text);
+            if (!isInnerCell)
+            {
+                tableElement.StartTag.AddAfterSelf(result);
+            }
+            else
+            {
+                if (currentCell.Elements(WordMl.ParagraphName).Any())
+                {
+                    currentCell.Elements(WordMl.ParagraphName).Remove();
+                }                 
+                currentCell.Add(result);
+            }
 
-            var staticCells = isInnerCell
-                                  ? tableElement.StartTag.ElementsBeforeSelf(WordMl.TableCellName).Where(element => element.IsBefore(previous)).ToList()
-                                  : currentCell.ElementsBeforeSelf(WordMl.TableCellName).Where(element => element.IsBefore(previous)).ToList();
+            if (previous != null && !previous.Equals(currentCell))
+            {
+                currentCell.Remove();
+                previous.AddAfterSelf(currentCell);
+            }
+            else if (previous == null)
+            {
+                var parentRow = isInnerCell ? tableElement.StartTag.Parent : currentCell.Parent;
+                currentCell.Remove();
+                parentRow.Add(currentCell);
+            }
+            
+            return currentCell;
+        }
+        
+        private XElement ProcessStaticCells(TableElement tableElement, XElement previous)
+        {
+            var currentElement = tableElement.HasCell()
+                                  ? tableElement.StartTag
+                                  : this.CurrentCell(tableElement);
+
+            return ProcessStaticCells(currentElement, previous);
+        }
+
+        private XElement ProcessStaticCells(XElement currentElement, XElement previous)
+        {
+            var staticCells =
+                currentElement.ElementsBeforeSelf(WordMl.TableCellName)
+                              .Where(element => element.IsBefore(previous))
+                              .ToList();
             if (staticCells.Any())
             {
                 if (previous == null)
@@ -158,49 +188,13 @@ namespace TsSoft.Docx.TemplateEngine.Tags.Processors
                 }
             }
 
-            if (!isInnerCell)
-            {
-                tableElement.StartTag.AddAfterSelf(result);
-                if (previous == null)
-                {
-                    var parentRow = currentCell.Parent;
-                    /*
-                    var textElements = currentCell.Elements(WordMl.TextRunName);
-                    
-                    foreach (var textElement in textElements )
-                    {
-                        var paragraph = currentCell.Element(WordMl.ParagraphName);                        
-                        textElement.Remove();
-                        paragraph.Add(textElement);
-                    }*/
-                    currentCell.Remove();
-                    parentRow.Add(currentCell);
-                }
-            }
-            else
-            {
-                
-                if (currentCell.Elements(WordMl.ParagraphName).Any())
-                {
-                    currentCell.Elements(WordMl.ParagraphName).Remove();
-                }                 
-                currentCell.Add(result);
-                if (previous == null)
-                {
-                    currentCell.Remove();
-                    tableElement.StartTag.Parent.Add(currentCell);
-                }
-            }
+            return previous;
+        }
 
-            if (previous != null && !previous.Equals(currentCell))
-            {
-                currentCell.Remove();
-                previous.AddAfterSelf(currentCell);
-            }
-
-            
-            
-            return currentCell;
+        private XElement CurrentCell(TableElement tableElement)
+        {
+            return tableElement.StartTag.Descendants(WordMl.TableCellName).FirstOrDefault()
+                   ?? tableElement.StartTag.Ancestors().First(element => element.Name == WordMl.TableCellName);
         }
 
         private void RemoveTags()
